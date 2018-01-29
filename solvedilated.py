2689e1b7d9b035f8b93ce409289d4f7ba489d39d
Sharif Amit Kamran <SharifAmit@users.noreply.github.com>
2018-01-29 14:45:12 +0600

    Update solvedilated.py

--- solvedilated.py
+++ solvedilated.py
@@ -9,21 +9,21 @@ setproctitle.setproctitle(os.path.basename(os.getcwd()))
 
 
 
-weights = 'Dilated_FCN-2s_VGG16/snapshot/'
+weights = 'Dilated_FCN-2s_VGG16/snapshot/vgg16surgery.caffemodel'
 # init
-#restoring ='voc-dil-rnn/snapshot/vocsbd_iter_235000.solverstate'
+#restoring ='Dilated_FCN-2s_VGG16/snapshot/voctraining_iter_100000.solverstate' #uncomment this out for resuming training  
 caffe.set_device(0)
 caffe.set_mode_gpu()
 
 solver = caffe.SGDSolver('voc-dil-rnn/solver.prototxt')
 solver.net.copy_from(weights)
-# solver.restore(restoring) #uncomment this out while restoring  
+# solver.restore(restoring) #uncomment this out while restoring for resuming training  
 
 interp_layers = [k for k in solver.net.params.keys() if 'up' in k] 
 surgery.interp(solver.net, interp_layers)  
 
 # scoring
-val = np.loadtxt('../seg12val.txt', dtype=str)
+val = np.loadtxt('seg12val.txt', dtype=str)
 
 for _ in range(20):
     solver.step(5000) 
